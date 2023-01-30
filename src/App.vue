b2c34105f99e3ec1199c8fd28872ad9b123226e9
Your Name <you@example.com>
2023-01-31 02:46:43 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  
+  <TestApp msg="test"/>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+//<img alt="Vue logo" src="./assets/logo.png">
+import TestApp from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
